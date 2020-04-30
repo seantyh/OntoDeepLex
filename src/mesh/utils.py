8896bb2d5d2244d42ade70b207a69cd67f63a497
seantyh <seantyh@gmail.com>
2020-04-30 16:12:46 +0800

    add sense_map

--- src/mesh/utils.py
+++ src/mesh/utils.py
@@ -7,3 +7,10 @@ def get_model_dir():
 def get_data_dir():
     return (Path(__file__).parent / "../../data")\
             .resolve().absolute()
+
+def ensure_dir(path: Path, path_is_dir=True):
+    if not path_is_dir:
+        dir_path = path.parent
+    else:
+        dir_path = path
+    dir_path.mkdir(parents=True, exist_ok=True)
